e8523c31b2bfbc85d8e05be0b7b4cace7553b77a
bonda29 <bondableglobe10@gmail.com>
2024-04-18 20:52:08 +0300

    initial commit

---
+++ src/main/java/tech/bonda/cft/CrossfadeThinkerApplication.java
@@ -0,0 +1,13 @@
+package tech.bonda.cft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossfadeThinkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrossfadeThinkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/bonda/cft/CrossfadeThinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.bonda.cft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossfadeThinkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
